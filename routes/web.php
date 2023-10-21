a51048e4ee65c6fe2de92d91ee1a9c1805634689
hackmack4772 <120358827+hackmack4772@users.noreply.github.com>
2023-10-22 00:38:19 +0530

    ishops first

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\admin\AdminHomeController;
+use App\Http\Controllers\admin\AdminLoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix("/admin")->group(function(){
+
+
+Route::group(["middleware"=>"admin.guest"],function(){
+    Route::get("login",[AdminLoginController::class,"index"])->name("admin.login");
+    Route::post("authenticate",[AdminLoginController::class,"authenticate"])->name("admin.authenticate");
+
+});
+Route::group(["middleware"=>"admin.auth"],function(){
+    Route::get("dashboard",[AdminHomeController::class,"index"])->name("admin.dashboard");
+    Route::get("logout",[AdminLoginController::class,"logout"])->name("admin.logout");
+
+});
 });
