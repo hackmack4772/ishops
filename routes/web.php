b150971fe92062c1a5db47ab7e35de8feeb844e0
hackmack4772 <120358827+hackmack4772@users.noreply.github.com>
2023-10-25 23:23:23 +0530

    =Changes

--- routes/web.php
+++ routes/web.php
@@ -18,19 +18,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
 Route::prefix("/admin")->group(function(){
-
-
 Route::group(["middleware"=>"admin.guest"],function(){
     Route::get("login",[AdminLoginController::class,"index"])->name("admin.login");
     Route::post("authenticate",[AdminLoginController::class,"authenticate"])->name("admin.authenticate");
-
 });
 Route::group(["middleware"=>"admin.auth"],function(){
     Route::get("dashboard",[AdminHomeController::class,"index"])->name("admin.dashboard");
     Route::get("logout",[AdminLoginController::class,"logout"])->name("admin.logout");
-
 });
 });
