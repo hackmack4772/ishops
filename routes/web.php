911483d1ac1f2d042d7603d7faf530cf546a9565
hackmack4772 <aamir.saleem@shinedezign.com>
2023-10-30 17:32:14 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -21,16 +21,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
 Route::prefix("/admin")->group(function(){
-
-
 Route::group(["middleware"=>"admin.guest"],function(){
     Route::get("login",[AdminLoginController::class,"index"])->name("admin.login");
     Route::post("authenticate",[AdminLoginController::class,"authenticate"])->name("admin.authenticate");
-
-
 });
 Route::group(["middleware"=>"admin.auth"],function(){
     Route::get("dashboard",[AdminHomeController::class,"index"])->name("admin.dashboard");
